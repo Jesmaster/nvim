a2e00311d79583b89a77744ef51411517cd7eb86
Jesmaster <jesmasterha@gmail.com>
2026-06-04 17:04:32 -0400

    Lazyvim 16 updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,6 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    "catppuccin",
     opts = {
       flavour = "mocha",
       transparent_background = true,
@@ -21,7 +19,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-nvim",
     },
   },
   {
